e4e2267a7683aab9795c294dd1a5c301f5524c28
Miguel Quintal <mquintal88@gmail.com>
2021-02-01 19:38:37 +0000

    Fix one to many types

--- src/entities/Draw.ts
+++ src/entities/Draw.ts
@@ -1,40 +1,47 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, Index, OneToMany } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Index,
+  OneToMany,
+} from "typeorm";
 import { Prizes } from ".";
 
 export enum DrawStatus {
-    IDLE = "idle",
-    VALID = "valid",
+  IDLE = "idle",
+  VALID = "valid",
 }
 
 @Entity()
 export class Draw {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column("int", { array: true })
+  numbers: number[];
 
-    @Column("int", { array: true })
-    numbers: number[];
+  @Column("int", { array: true })
+  stars: number[];
 
-    @Column("int", { array: true })
-    stars: number[];
+  @Index()
+  @Column({ type: "date" })
+  date: Date;
 
-    @Index()
-    @Column({ type: 'date' })
-    date: Date
+  @Column({
+    type: "enum",
+    enum: DrawStatus,
+    default: DrawStatus.IDLE,
+  })
+  status: DrawStatus;
 
-    @Column({
-        type: "enum",
-        enum: DrawStatus,
-        default: DrawStatus.IDLE
-    })
-    status: DrawStatus;
+  @OneToMany(() => Prizes, (prizes) => prizes.draw)
+  prizes: Prizes[];
 
-    @OneToMany(() => Prizes, prizes => prizes.draw)
-    prizes: Prizes;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @CreateDateColumn()
-    created_at: Date
-
-    @UpdateDateColumn()
-    updated_at: Date
+  @UpdateDateColumn()
+  updated_at: Date;
 }
